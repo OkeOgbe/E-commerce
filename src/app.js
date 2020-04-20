0ab09dc221cc704e423edbce37dbceeb6e6e6433
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2020-04-20 22:37:08 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+$(window).on("scroll", function(){
+      if ($(this).scrollTop() > 100) {
+          $("nav").addClass("fixed_nav");
+      } else {
+          $("nav").removeClass("fixed_nav");
+      }
+});
