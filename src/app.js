941d0339710464ac8a2e0a683fa4c13b93f037bb
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2020-04-21 10:56:02 +0100

    dashboard pages initial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
-$(window).on("scroll", function(){
-      if ($(this).scrollTop() > 100) {
-          $("nav").addClass("fixed_nav");
-      } else {
-          $("nav").removeClass("fixed_nav");
-      }
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 100) {
+    $("nav").addClass("fixed_nav");
+  } else {
+    $("nav").removeClass("fixed_nav");
+  }
 });
+
+$('.dropdown').on('show.bs.dropdown', function () {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+$('.dropdown').on('hide.bs.dropdown', function () {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
